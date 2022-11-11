009aabdc54faf378fb12ee112a17116923d3214a
Eduardo Almeida da Vitória <115654044+Eduardoadavitoria@users.noreply.github.com>
2022-11-11 23:26:20 +0000

    Visual function was created, responsible for the visual part of the system.

--- Gerenciamento de aviao.cs
+++ Gerenciamento de aviao.cs
@@ -1,1 +1,43 @@
-//html ou txt
+using System;
+
+class Gerenciamento {
+  public static void Main (string[] args) {
+//Função que chama a parte gráfica do sistema
+  Visual();
+
+  }
+
+public static void Visual()
+{
+  string opcao;
+  bool ativo = true;
+  while(ativo){
+  Console.WriteLine("===========================");
+  Console.WriteLine("= Bem vindo a Aviação 404 =");
+  Console.WriteLine("===========================");
+  Console.WriteLine();
+  Console.WriteLine("- Escolha a opção desejada");
+  Console.WriteLine("0 - Sair");
+  Console.WriteLine("1 - Adicionar passageiro");
+  opcao = Console.ReadLine();
+  
+  if(opcao == "0"){
+    ativo = false;
+    Environment.Exit(1);
+  }
+  if(opcao == "1"){
+    
+    Console.WriteLine("Passageiro adicionado");
+    ativo = false;
+  }
+  if(opcao == "2"){
+    Console.Clear();
+  }
+    
+  }
+}
+
+
+  
+}
+
